ff573b10dba525aadf4cdfe1aacc0c7187430ce2
Richard Choi <richardchoi54@gmail.com>
2024-03-20 13:38:58 -0400

    organize code with mvc and oop modeling, and add error handlers

---
+++ controllers/blockchain.js
@@ -0,0 +1,63 @@
+const rp = require("request-promise");
+const Blockchain = require("../blockchain");
+const bitcoin = new Blockchain();
+const handleError = require("../config/Errors");
+const logValue = require("../config/Test");
+
+module.exports = {
+  //Get entire blockchain
+  getBlockchain: async function (req, res) {
+    try {
+      const blockchain = await res.send(bitcoin);
+      logValue(blockchain);
+    } catch (err) {
+      handleError(err);
+    }
+  },
+  makeTransaction: async function (req, res) {
+    try {
+      const newTransaction = req.body;
+      const blockIndex =
+        bitcoin.addtransactionToPendingTransactions(newTransaction);
+      logValue(blockIndex);
+      logValue(newTransaction);
+      res.json({ note: "Transaction will be added in block ${blockINdex}." });
+    } catch (err) {
+      handleError(err);
+    }
+  },
+  broadcastTransaction: async function (req, res) {
+    try {
+      const newTransaction = bitcoin.createNewTransaction(
+        req.body.amount,
+        req.body.sender,
+        req.body.recipient,
+      );
+      bitcoin.addtransactionToPendingTransactions(newTransaction);
+
+      logValue(newTransaction);
+
+      const requestPromises = [];
+      bitcoin.networkNodes.forEach((networkNodeUrl) => {
+        const requestOptions = {
+          uri: networkNodeUrl + "/transaction",
+          method: "POST",
+          body: newTransaction,
+          json: true,
+        };
+
+        logValue(requestOptions);
+        requestPromises.push(rp(requestOptions));
+      });
+
+      logValue(requestPromises);
+
+      Promise.all(requestPromises).then((data) => {
+        logValue(data);
+        res.json({ note: "Transaction created and broadcast successfully." });
+      });
+    } catch (err) {
+      handleError(err);
+    }
+  },
+};
